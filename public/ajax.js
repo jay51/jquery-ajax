fd3cae96041be457b92984060a6f135de98f1dc6
jay51 <nenojaafar3@gmail.com>
2018-10-10 23:51:19 -0500

    added code to send put and delete requests but it's still not complete

--- public/ajax.js
+++ public/ajax.js
@@ -3,13 +3,41 @@
 // 	debugger;
 // });
 
-// listen for form submit
+// listen for form submit POST
+// $("form").submit(function(e) {
+// 	e.preventDefault();
+// 	// converte the data to string
+// 	const formData = $(this).serialize();
+// 	// send post to same domain post route
+// 	$.post("/todos", formData, function(res) {
+// 		console.log("The data we got back:", res);
+// 	});
+// });
+
+// // listen for form submit PUT
+// $("form").submit(function(e) {
+// 	e.preventDefault();
+// 	const formData = $(this).serialize();
+// 	const formAction = $(this).attr("action");
+// 	$.ajax({
+// 		url: formAction,
+// 		data: formData,
+// 		type: "PUT",
+// 		success: function(data) {
+// 			debugger;
+// 		}
+// 	});
+// });
+
+// listen for form submit DELETE
 $("form").submit(function(e) {
 	e.preventDefault();
-	// converte the data to string
-	const formData = $(this).serialize();
-	// send post to same domain post route
-	$.post("/todos", formData, function(res) {
-		console.log("The data we got back:", res);
+	const formAction = $(this).attr("action");
+	$.ajax({
+		url: formAction,
+		type: "DELETE",
+		success: function(data) {
+			debugger;
+		}
 	});
 });
