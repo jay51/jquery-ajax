f3050bdb8ad46597e266651bcad506816a955093
jay51 <nenojaafar3@gmail.com>
2018-10-17 17:35:06 -0500

    moved applecation into server api and a client app

--- client/ajax.js
+++ client/ajax.js
@@ -1,7 +1,35 @@
-// $.get("/todos", function(data) {
-// 	// debugger opens chrome debuging tools when it hits this point
-// 	debugger;
-// });
+// open the clinet app with VS code live server
+$.get("http://localhost:3000/todos", function(todos) {
+	todos.forEach(function(todo) {
+		$("#todo-list").append(
+			`<li class="list-group-item">
+			
+			<form id="edit-todo-form" action="/todos/${todo._id}" method="POST">
+			<div class="form-group">
+			<label for="${todo._id}" >Item Text</label>
+			<input type="text" value="${todo.text}" name="todo[text]" id="${
+				todo._id
+			}" class="form-control">
+			</div>
+			<button class="btn btn-primary">Update Item</button>
+			</form>
+			
+			<span class="lead">
+			${todo.text}
+			</span>
+			<div class="pull-right">
+			<button id="edit-btn" class="btn btn-sm btn-warning">Edit</button>
+			<form id="delete-todo-form" style="display: inline" method="POST" action="/todos/${
+				todo._id
+			}">
+			<button type="submit" class="btn btn-sm btn-danger">Delete</button>
+			</form>
+			</div>
+			<div class="clearfix"></div>
+			</li>`
+		);
+	});
+});
 
 // listen for form submit POST
 $("#new-todo-form").submit(function(e) {
@@ -9,7 +37,7 @@ $("#new-todo-form").submit(function(e) {
 	// converte the data to string
 	const todoItem = $(this).serialize();
 	// send post to same domain post route
-	$.post("/todos", todoItem, function(todo) {
+	$.post("http://localhost:3000/todos", todoItem, function(todo) {
 		// update the todos ul
 		$("#todo-list").append(
 			`<li class="list-group-item">
@@ -58,7 +86,7 @@ $("#todo-list").on("click", "#edit-btn", function() {
 $("#todo-list").on("submit", "#edit-todo-form", function(e) {
 	e.preventDefault();
 	const formData = $(this).serialize();
-	const formAction = $(this).attr("action");
+	const formAction = "http://localhost:3000" + $(this).attr("action");
 	const originalTodo = $(this).parent(".list-group-item");
 	$.ajax({
 		url: formAction,
@@ -99,7 +127,7 @@ $("#todo-list").on("submit", "#delete-todo-form", function(e) {
 	const confirmRes = confirm("Are you sure?");
 	if (confirmRes) {
 		const originalTodo = $(this).closest(".list-group-item");
-		const formAction = $(this).attr("action");
+		const formAction = "http://localhost:3000" + $(this).attr("action");
 		$.ajax({
 			url: formAction,
 			type: "DELETE",
--- server/app.js
+++ server/app.js
@@ -2,18 +2,24 @@ var express = require("express"),
 	app = express(),
 	mongoose = require("mongoose"),
 	bodyParser = require("body-parser"),
-	expressSanitizer = require("express-sanitizer"),
-	methodOverride = require("method-override");
+	expressSanitizer = require("express-sanitizer");
 
 mongoose.connect(
 	"mongodb://localhost:27017/todo_app",
 	{ useNewUrlParser: true }
 );
-app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressSanitizer());
-app.set("view engine", "ejs");
-app.use(methodOverride("_method"));
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Cotnent-Type, Accept"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	next();
+});
 
 var todoSchema = new mongoose.Schema({
 	text: String
@@ -33,18 +39,9 @@ app.get("/todos", function(req, res) {
 		if (err) {
 			console.log(err);
 		} else {
-			// if request was sent vi ajax by checking the request header & send json back
-			if (req.xhr) {
-				res.json(todos);
-			} else {
-				res.render("index", { todos: todos });
-			}
+			res.json(todos);
 		}
 	});
-});
-
-app.get("/todos/new", function(req, res) {
-	res.render("new");
 });
 
 app.post("/todos", function(req, res) {
@@ -57,17 +54,6 @@ app.post("/todos", function(req, res) {
 			res.render("new");
 		} else {
 			res.json(newTodo);
-		}
-	});
-});
-
-app.get("/todos/:id/edit", function(req, res) {
-	Todo.findById(req.params.id, function(err, todo) {
-		if (err) {
-			console.log(err);
-			res.redirect("/");
-		} else {
-			res.render("edit", { todo: todo });
 		}
 	});
 });
@@ -98,8 +84,3 @@ app.delete("/todos/:id", function(req, res) {
 app.listen(3000, function() {
 	console.log("Server running on port 3000");
 });
-
-// // Uncomment the three lines of code below and comment out or remove lines 84 - 86 if using cloud9
-// app.listen(process.env.PORT, process.env.IP, function(){
-//     console.log("The server has started!");
-// });
